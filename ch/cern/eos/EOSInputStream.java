9f2b16592a69129cd457df2c28d9db36f81afa24
Zbigniew Baranowski <zbigniew.baranowski@cern.ch>
2018-01-26 12:02:15 +0000

    Fixing read method semantics in EOSInputStream.java

---
+++ ch/cern/eos/EOSInputStream.java
@@ -0,0 +1,124 @@
+package ch.cern.eos;
+
+import java.io.DataInputStream;
+import java.io.FilterInputStream;
+//import java.io.InputStream;
+import org.apache.hadoop.fs.FSInputStream;
+//
+import java.io.IOException;
+import java.io.EOFException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.PositionedReadable;
+import org.apache.hadoop.fs.Seekable;
+
+import ch.cern.eos.XrdClFile;
+
+class EOSInputStream extends FSInputStream implements Seekable, PositionedReadable {
+
+    private static final Log LOG = LogFactory.getLog(EOSInputStream.class);
+    private XrdClFile file;
+//    private boolean EOS_debug = false;
+    private boolean EOS_debug = false;
+
+    private long pos;
+
+    public EOSInputStream(String url) {
+	if (System.getenv("EOS_debug") != null) EOS_debug = true;
+
+	file = new XrdClFile();
+	long status = file.Open(url, 0, 0);
+	if (status != 0) System.out.println("open " + url + " status=" + status);
+	    this.pos = 0;
+    }
+
+    public long getPos() {
+	return pos;
+    }
+
+    public int read() throws IOException{
+//Reads the next byte of data from the input stream.
+        byte[] b = new byte[1];
+
+        long rd = read(this.pos, b, 0, 1);
+        if (rd > 0) return b[0] & 0xFF;
+        if (rd == -1) return -1;
+        
+        throw new IOException("read returned " + rd);
+    }
+
+    public int read(byte[] b, int off, int len) throws IOException {
+        return read(this.pos, b, off, len);
+    }
+
+    public int read(long pos, byte[] b, int off, int len) throws IOException {
+        //this.pos = pos;
+        if (b.length == 0 ) return 0;
+
+        if (this.pos < 0) { //?????
+            if (EOS_debug) System.out.println("EOSInputStream.read() pos: " + this.pos);
+
+            //if (this.pos == -1) throw new EOFException();
+            return (int) this.pos;
+        }
+
+        long rd = file.Read(pos, b, off, len);
+        if (EOS_debug) System.out.println("EOSInputStream.read() bytes: " + rd);
+        if (rd >= 0) {
+            this.pos = pos + rd;
+            return (int) rd;
+        }
+        else if (rd == -1 ){
+            this.pos = -1;
+            return -1;
+        }
+
+        this.pos = -2;
+        throw new IOException("read returned " + rd);
+    }
+
+    public void readFully(long pos, byte[] b) throws IOException {
+        int apos=0;
+        int io_size=1024*1024;
+        //this.pos=pos;
+        if (EOS_debug) System.out.println("EOSInputStream.(long pos, byte[] b) pos: " + pos);
+        while(this.pos>=0)
+	    {
+	       byte[] a = new byte[io_size];
+	       long rd = read(pos,a,0,io_size);
+	       pos = this.pos;
+           System.arraycopy(a, 0, b, apos, (int)rd);
+            apos+=rd;
+        }
+        if (EOS_debug) System.out.println("EOSInputStream.(long pos, byte[] b) bytes read: " + apos);
+
+//	throw new IllegalArgumentException("readFully(pos, b) not implemented");
+    }
+
+    public void readFully(long pos, byte[] buffer, int off, int len) throws IOException {
+        if (EOS_debug) System.out.println("EOSInputStream.readFully(long pos, byte[] buffer, int off, int len) pos: " + pos);
+
+        read(pos,buffer,off,len);
+//	throw new IllegalArgumentException("readFully(pos, b, off, len) not implemented");
+    }
+
+    public void seek(long pos) {
+	this.pos = pos;
+    }
+
+    public boolean seekToNewSource(long targetPos) {
+	throw new IllegalArgumentException("seekToNewSource");
+    }
+
+    public void close() throws IOException {
+	if (pos < -1) return;
+
+	long st = file.Close();
+	if (st != 0) System.out.println("close(): " + st);
+	pos = -2;
+    }
+
+}
--- src/ch/cern/eos/XrootDBasedInputStream.java
+++ src/ch/cern/eos/XrootDBasedInputStream.java
@@ -46,7 +46,7 @@ class XrootDBasedInputStream extends FSInputStream implements Seekable, Position
         // Reads the next byte of data from the input stream.
         byte[] b = new byte[1];
 
-        long rd = read(pos, b, 0, 1);
+        long rd = read(this.pos, b, 0, 1);
         if (rd > 0) {
         	return b[0] & 0xFF;
         }
@@ -62,24 +62,22 @@ class XrootDBasedInputStream extends FSInputStream implements Seekable, Position
     }
 
     public int read(long pos, byte[] b, int off, int len) throws IOException {
-        this.pos = pos;
+        //this.pos = pos;
 
-        if (this.pos < 0) {
+        if (this.pos < 0) {  // TODO: This is unclear
             this.eosDebugLogger.printDebug("EOSInputStream.read() pos: " + this.pos);
 
-            if (this.pos == -1) {
-            	throw new EOFException();
-            }
+            //if (this.pos == -1) { throw new EOFException(); }
             return (int)this.pos;
         }
 
-        long rd = file.Read(this.pos, b, off, len);
+        long rd = file.Read(pos, b, off, len);
         this.eosDebugLogger.printDebug("EOSInputStream.read() bytes: " + rd);
         if (rd >= 0) {
-            this.pos += rd;
-            if (rd > 0) {
-            	return (int) rd;
-            }
+            this.pos = pos + rd;
+            return (int) rd;
+        }
+        else if (rd == -1 ){
             this.pos = -1;
             return -1;
         }
@@ -96,7 +94,8 @@ class XrootDBasedInputStream extends FSInputStream implements Seekable, Position
         while (this.pos >= 0)
         {
             byte[] a = new byte[IO_SIZE];
-            long rd = read(this.pos, a, 0, IO_SIZE);
+            long rd = read(pos,a,0, IO_SIZE);
+            pos = this.pos;
             System.arraycopy(a, 0, b, apos, (int)rd);
             apos += rd;
         }
